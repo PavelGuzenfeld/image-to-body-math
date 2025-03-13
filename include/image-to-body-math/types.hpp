752509960d23aa7ff49e837c05360465c748d2bd
Pavel Guzenfeld <me@pavelguzenfeld.com>
2025-03-13 19:54:46 +0200

    Add PixelIndex class and update ImageSize structure; enhance tests for pixel_tan_from_fov functionality

--- include/image-to-body-math/types.hpp
+++ include/image-to-body-math/types.hpp
@@ -5,7 +5,7 @@
 namespace p2b
 {
 
-    struct Size2D
+    struct ImageSize
     {
         uint64_t width{}, height{};
 
@@ -17,6 +17,22 @@ namespace p2b
         [[nodiscard]] constexpr double half_height() const noexcept
         {
             return static_cast<double>(height) / 2.0;
+        }
+    };
+
+    class PixelIndex
+    {
+    public:
+        uint64_t value;
+
+        explicit constexpr PixelIndex(uint64_t value) noexcept
+            : value(value)
+        {
+        }
+
+        [[nodiscard]] constexpr double normalized(const ImageSize &size) const noexcept
+        {
+            return static_cast<double>(value) / size.half_width() - 1.0;
         }
     };
 
