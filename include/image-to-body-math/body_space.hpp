32ae90339658878ef240036d3d0967e4c394cf1e
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2026-03-09 10:44:10 +0200

    Fix: use linalg3d::clamp instead of std::clamp

--- include/image-to-body-math/body_space.hpp
+++ include/image-to-body-math/body_space.hpp
@@ -237,7 +237,7 @@ using Quaternion = linalg3d::Quaternion;
                                                 PixelToTan pixel_to_tan) noexcept
 {
     const double d = ned1.normalized().dot(ned2.normalized());
-    const double angle = std::acos(std::clamp(d, -1.0, 1.0));
+    const double angle = std::acos(linalg3d::clamp(d, -1.0, 1.0));
     return std::tan(angle) / pixel_to_tan.get();
 }
 
